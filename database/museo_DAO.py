3a49b6330fd10ca812fa7c0a4a7e83973d83be09
Riccardo-cmd <s341875@studenti.polito.it>
2025-11-05 14:29:51 +0100

    prime modifiche in lab

--- database/museo_DAO.py
+++ database/museo_DAO.py
@@ -1,3 +1,5 @@
+import mysql
+
 from database.DB_connect import ConnessioneDB
 from model.museoDTO import Museo
 
@@ -11,3 +13,20 @@ class MuseoDAO:
         pass
 
     # TODO
+
+    def get_all_musei(self):
+        cnx = ConnessioneDB().get_connection()
+
+        cursor = cnx.cursor()
+        cursor.execute("SELECT * FROM museo")
+
+        result = []
+
+        for row in cursor:
+            museo = Museo(row[0], row[1], row[2])
+            result.append(museo)
+
+        cursor.close()
+        cnx.close()
+
+        return result
